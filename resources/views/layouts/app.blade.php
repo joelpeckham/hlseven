0ff29f617bf8ac4fe147df44e26168e4a2d6da90
Joel Peckham <mail@jpeckham.com>
2023-04-26 20:26:22 +0000

    tooling around with blade layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <title>@yield('title')</title>
+    {{-- Styles --}}
+    <link href="{{ asset('vendor/bladewind/css/animate.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('vendor/bladewind/css/bladewind-ui.min.css') }}" rel="stylesheet" />
+    @livewireStyles
+
+    {{-- Scripts --}}
+    <script src="{{ asset('vendor/bladewind/js/helpers.js') }}"></script>
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    {{-- Alpine JS only needed for bladewind datepicker and timepicker --}}
+</head>
+
+<body>
+    <livewire:nav />
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    @livewireScripts
+</body>
+
+</html>
